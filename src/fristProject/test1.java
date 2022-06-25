4f6305a5ef1b200b8a5613d55dc6cd2f0e101171
seongho <seonghorjatk@naver.com>
2022-06-25 17:15:47 +0900

    "first commit and make hi"

---
+++ src/fristProject/test1.java
@@ -0,0 +1,16 @@
+package fristProject;
+
+class Hi{
+	void a() {
+		System.out.println("Hi");
+	}
+}
+public class test1 {
+
+	public static void main(String[] args) {
+		Hi hi = new Hi();
+		hi.a();
+
+	}
+
+}
